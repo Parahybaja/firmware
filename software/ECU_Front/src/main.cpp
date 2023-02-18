9219286792c9869840b2d5d8428fa7624b9e9e30
Jefferson-Lopes <jeff.777.lopes@gmail.com>
2023-02-18 16:22:47 -0300

    add include from folder

--- software/ECU_Front/src/main.cpp
+++ software/ECU_Front/src/main.cpp
@@ -2,8 +2,8 @@
  * @file ECU_Front.ino
  * @author jefferson lopes (jefferson.lopes@ee.ufcg.edu.br)
  * @brief Parahybaja's embedded system: Front ECU
- * @version 3.0
- * @date 2023-02-05
+ * @version 3.1
+ * @date 2023-02-18
  * 
  * @copyright Copyright (c) 2023
  * 
@@ -11,28 +11,26 @@
 
 #include <Arduino.h>
 
+// common libs
 #include "communication.h"
+#include "tasks/task_telemetry.h"
+#include "tasks/task_battery.h"
+#include "tasks/task_display_LCD.h"
+#include "tasks/task_alive.h"
+
 #include "espnow_callback.h"
-#include "task_telemetry.h"
-#include "task_battery.h"
-#include "task_display_LCD.h"
-#include "task_alive.h"
 
 void setup(){
     // init system general modules (pinMode and Serial)
     init_system();
 
     // -----header-----
-    Serial.println("|-------------------------|");
-    Serial.println("|                         |");
-    Serial.println("|        ECU_Front        |");
-    Serial.println("|                         |");
-    Serial.println("|-------------------------|");
+    log_i("embedded system: ECU Front");
 
     // -----create semaphores-----
     sh_global_vars = xSemaphoreCreateMutex();
     if(sh_global_vars == NULL)
-        ERROR("ERROR_1: global vars Semaphore init failed", false);
+        ERROR("global vars Semaphore init failed", false);
 
     // -----create queues-----
     qh_speed     = xQueueCreate(QUEUE_BUFFER_SIZE, sizeof(sensor_t));
@@ -48,7 +46,7 @@ void setup(){
     esp_now_register_recv_cb(OnDataRecv);
 
     // -----final confirmation-----
-    INFO("INFO_1: it's all configured!");
+    INFO("it's all configured!");
     delay(50); // give time to send the espnow message
 
     // -----fire up tasks-----
