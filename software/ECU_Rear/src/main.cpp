0181185e046212aa766c39dfde53d88aa77d85ce
Jefferson-Lopes <jeff.777.lopes@gmail.com>
2023-02-19 16:58:01 -0300

    add fuel task and move pinMode

--- software/ECU_Rear/src/main.cpp
+++ software/ECU_Rear/src/main.cpp
@@ -2,8 +2,8 @@
  * @file ECU_Rear.ino
  * @author jefferson lopes (jefferson.lopes@ee.ufcg.edu.br)
  * @brief Parahybaja's embedded system: Rear ECU
- * @version 3.0
- * @date 2023-02-10
+ * @version 3.1
+ * @date 2023-02-19
  * 
  * @copyright Copyright (c) 2023
  * 
@@ -13,21 +13,34 @@
 
 #include "communication.h"
 #include "espnow_callback.h"
-#include "task_speedometer.h"
-#include "task_battery.h"
-#include "task_RPM.h"
-#include "task_alive.h"
+#include "tasks/task_speedometer.h"
+#include "tasks/task_battery.h"
+#include "tasks/task_fuel.h"
+#include "tasks/task_RPM.h"
+#include "tasks/task_alive.h"
 
-void setup(){
+// ----- pinout -----
+const uint8_t pin_rpm = 4;
+const uint8_t pin_fuel = 5;
+const uint8_t pin_battery = 35;
+const uint8_t pin_alive_LED = 2;
+const uint8_t pin_speedometer = 17;
+
+void setup() {
     // init system general modules (pinMode and Serial)
     init_system();
 
     // -----header-----
-    Serial.println("|------------------------|");
-    Serial.println("|                        |");
-    Serial.println("|        ECU_Rear        |");
-    Serial.println("|                        |");
-    Serial.println("|------------------------|");
+    log_i("embedded system: ECU Rear");
+
+    pinMode(pin_rpm, INPUT);
+    pinMode(pin_fuel, INPUT);
+    pinMode(pin_battery, INPUT);
+    pinMode(pin_alive_LED, OUTPUT);
+    pinMode(pin_speedometer, INPUT);
+
+    attachInterrupt(pin_speedometer, isr_spdmt, CHANGE);
+    attachInterrupt(pin_rpm, isr_rpm, HIGH);
 
     // init system handlers such as queues and semaphores
     init_espnow();
@@ -37,21 +50,18 @@ void setup(){
     esp_now_register_recv_cb(OnDataRecv);
 
     // -----final confirmation-----
-    INFO("INFO_1: it's all configured!");
-    delay(50); // give time to send the espnow message
+    INFO("it's all configured!");
 
     // -----fire up tasks-----
-    xTaskCreatePinnedToCore(
+    xTaskCreatePinnedToCore( // 
         task_battery,   // task function
         "task_battery", // task name
         2048,           // stack size
         NULL,           // parameters
-        10,             // priority
+        9,              // priority
         &th_battery,    // handler
         APP_CPU_NUM     // core number
     ); 
-
-    vTaskDelay(50 / portTICK_PERIOD_MS); // give time to send the espnow message
     
     xTaskCreatePinnedToCore(
         task_RPM,   // task function
@@ -63,24 +73,30 @@ void setup(){
         APP_CPU_NUM // core number
     ); 
 
-    vTaskDelay(50 / portTICK_PERIOD_MS); // give time to send the espnow message
+    xTaskCreatePinnedToCore(
+        task_fuel,   // task function
+        "task fuel", // task name
+        2048,        // stack size
+        NULL,        // parameters
+        11,          // priority
+        &th_fuel,    // handler
+        APP_CPU_NUM  // core number
+    ); 
 
     xTaskCreatePinnedToCore(
         task_speedometer,   // task function
         "task speedometer", // task name
         2048,               // stack size
         NULL,               // parameters
-        10,                 // priority
+        12,                 // priority
         &th_speedometer,    // handler
         APP_CPU_NUM         // core number
     ); 
-
-    vTaskDelay(50 / portTICK_PERIOD_MS); // give time to send the espnow message
     
     xTaskCreatePinnedToCore(
         task_alive_LED, // task function
         "alive LED",    // task name
-        1024,           // stack size
+        2048,           // stack size
         NULL,           // parameters
         5,              // priority
         &th_alive,      // handler
@@ -88,6 +104,6 @@ void setup(){
     );   
 }
 
-void loop(){
+void loop() {
     vTaskDelete(NULL);
 }
