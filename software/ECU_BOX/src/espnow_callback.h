d96bd662a87ddf9d93729650105cc446bf2b8287
Jefferson Lopes <jeff.777.lopes@gmail.com>
2023-02-21 14:39:03 -0300

    update espnow callback log

--- software/ECU_BOX/src/espnow_callback.h
+++ software/ECU_BOX/src/espnow_callback.h
@@ -10,11 +10,11 @@
  * @param status esp-now status
  */
 void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status){
-#if DEBUG_MODE
     // -----check if the data was delivered-----
-    Serial.print("Send status:\t");
-    Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
-#endif
+    if (status == ESP_NOW_SEND_SUCCESS)
+        log_i("Send status:\tDelivery Success");
+    else
+        log_e("Send status:\tDelivery Fail");
 }
 
 /**
