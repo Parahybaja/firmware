59e98376ff59313a3dc218a36ea8c743426e04d1
Jefferson Lopes <jefferson.lopes@ee.ufcg.edu.br>
2024-01-12 17:24:45 -0300

    add working extra dir example

---
+++ components/new_mod/new_mod.c
@@ -0,0 +1,32 @@
+/**
+ * @file system.h
+ * @author Jefferson Lopes (jefferson.lopes@ufcg.edu.br)
+ * @brief system's general types definitions and basic esp-now communication functions
+ * @version 2.0
+ * @date 2024-01-08
+ *
+ * @copyright Copyright (c) 2023
+ *
+ * Version   Modified By   Date        Comments
+ * -------  -------------  ----------  -----------
+ *  0.1.0    Jefferson L.  10/02/2023  first version
+ *  1.0.0    Jefferson L.  18/07/2023  move common_libs and rename a few modules
+ *  1.1.0    Jefferson L.  26/10/2023  remove Wire begin
+ *  2.0.0    Jefferson L.  08/01/2024  esp-idf convertion
+ * 
+ */
+
+#ifndef __MOD_H__
+#define __MOD_H__
+
+#include <stdio.h>
+#include "new_mod.h"
+
+int test2 = 4;
+
+void func(void)
+{
+
+}
+
+#endif // __SYSTEM_H__
---
+++ test-dir/main/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+#include "system.h"
+#include "new_mod.h"
+
+void app_main(void)
+{
+    func2();
+}
