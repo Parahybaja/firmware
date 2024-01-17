57251e15fbae2a2c763d9b1d21916bf0ffe89e1c
Jefferson Lopes <jefferson.lopes@ee.ufcg.edu.br>
2024-01-17 14:23:17 -0300

    add gpio config

--- components/fuel_em/src/fuel_em.c
+++ components/fuel_em/src/fuel_em.c
@@ -3,10 +3,27 @@
 static const char *TAG = "task_fuel_em";
 
 void task_fuel_em(void *arg){
-    (void)arg;
+    
+    const gpio_num_t gpio_pin = (gpio_num_t)arg;
+
+    // -----config gpio-----
+    //zero-initialize the config structure.
+    gpio_config_t io_conf = {};
+    //disable interrupt
+    io_conf.intr_type = GPIO_INTR_DISABLE;
+    //set as output mode
+    io_conf.mode = GPIO_MODE_INPUT;
+    //bit mask of the pins that you want to set
+    io_conf.pin_bit_mask = (1ULL<<gpio_pin);
+    //disable pull-down mode
+    io_conf.pull_down_en = false;
+    //disable pull-up mode
+    io_conf.pull_up_en = false;
+    //configure GPIO with the given settings
+    ESP_ERROR_CHECK(gpio_config(&io_conf)); 
 
     // create task variables
-    const int send_rate = int(1000.0 / float(TASK_FUEL_SEND_RATE_Hz));
+    const int send_rate = (int)(1000.0 / (float)(TASK_FUEL_SEND_RATE_Hz));
     uint32_t timer_send;
     float sum;
     bool last_value = false;
@@ -27,11 +44,11 @@ void task_fuel_em(void *arg){
             sum = 0; // clean sum buffer
             for (int i=0; i < FUEL_AVERAGE_POINTS; i++) { 
                 if (ACTIVE_LOW)
-                    sum += !digitalRead(pin_fuel);
+                    sum += !gpio_get_level(gpio_pin);
                 else
-                    sum += digitalRead(pin_fuel);
+                    sum += gpio_get_level(gpio_pin);
             }
-            fuel.value = sum / float(FUEL_AVERAGE_POINTS);
+            fuel.value = sum / (float)(FUEL_AVERAGE_POINTS);
 
             // ----- define high or low level -----
             // comment out to send raw data fuel average
