8d4d4a55a8ef15ec4ea59277acc5298d60ab2381
Jefferson Lopes <jefferson.lopes@ee.ufcg.edu.br>
2024-02-21 16:19:09 -0300

    fix battery task call

--- ECU_rear/main/main.c
+++ ECU_rear/main/main.c
@@ -17,42 +17,58 @@
 #include <stdio.h>
 #include "system.h"
 #include "task/alive.h"
-#include "task/fuel_em.h"
+#include "task/battery.h"
+// #include "task/fuel_em.h"
 
 #include "espnow_callback.h"
 
 static const char *TAG = "ECU_rear";
 
 static const gpio_num_t alive_pin = GPIO_NUM_2;
-static const gpio_num_t fuel_em_pin = GPIO_NUM_34;
+// static const gpio_num_t fuel_em_pin = GPIO_NUM_34;
+static const battery_config_t battery_config = {
+    .adc_channel = ADC_CHANNEL_7, // GPIO 35
+    .R1 = 20e3,
+    .R2 = 10e3
+};
 
 void app_main(void) {
     
     ESP_LOGD(TAG, "ECU rear v5");
 
-    init_espnow();
+    system_espnow_init();
     register_callbacks();
 
     print_mac_address();
 
     // -----fire up tasks-----
     xTaskCreatePinnedToCore(
-        task_alive_LED,  // task function
-        "alive LED",     // task name
-        2048,            // stack size
+        task_alive_LED,   // task function
+        "alive LED",      // task name
+        2048,             // stack size
         (void*)alive_pin, // parameters
-        8,               // priority
-        &th_alive,       // handler 
-        APP_CPU_NUM      // core number
+        8,                // priority
+        &th_alive,        // handler 
+        APP_CPU_NUM       // core number
     );
 
     xTaskCreatePinnedToCore(
-        task_fuel_em,       // task function
-        "fuel emergency",   // task name
-        4096,               // stack size
-        (void*)fuel_em_pin, // parameters
-        8,                  // priority
-        &th_fuel_em,        // handler 
-        APP_CPU_NUM         // core number
+        task_battery,          // task function
+        "battery",             // task name
+        2048,                  // stack size
+        (void*)&battery_config, // parameters
+        8,                     // priority
+        &th_battery,           // handler 
+        APP_CPU_NUM            // core number
     );
+
+    // xTaskCreatePinnedToCore(
+    //     task_fuel_em,       // task function
+    //     "fuel emergency",   // task name
+    //     4096,               // stack size
+    //     (void*)fuel_em_pin, // parameters
+    //     8,                  // priority
+    //     &th_fuel_em,        // handler 
+    //     APP_CPU_NUM         // core number
+    // );
 }
