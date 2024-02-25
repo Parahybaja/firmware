f3fb892df22a154d4181637db7b80f5452a0fe33
Jefferson Lopes <jefferson.lopes@ee.ufcg.edu.br>
2024-02-25 10:23:09 -0300

    create first working version

--- test_i2c/main/main.c
+++ test_i2c/main/main.c
@@ -1,24 +1,21 @@
 #include <stdio.h>
-#include <string.h>
-#include "sdkconfig.h"
 #include "esp_log.h"
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
 #include "driver/i2c.h"
 
+static const char *TAG = "test-i2c";
 
-static const char *TAG = "i2c-simple-example";
+/*----- i2c config-----*/
+#define I2C_MASTER_SCL_IO 22               /*!< gpio number for I2C master clock */
+#define I2C_MASTER_SDA_IO 21               /*!< gpio number for I2C master data  */
+#define I2C_MASTER_PORT_NUM I2C_NUM_0      /*!< I2C port number for master dev */
+#define I2C_MASTER_FREQ_HZ 400e3           /*!< 100kHz - 2C master clock frequency */
+#define I2C_MASTER_TX_BUF_DISABLE 0        /*!< I2C master doesn't need buffer */
+#define I2C_MASTER_RX_BUF_DISABLE 0        /*!< I2C master doesn't need buffer */
+#define ACK_CHECK_EN 0x1                   /*!< I2C master will check ack from slave*/
+#define ACK_CHECK_DIS 0x0                  /*!< I2C master will not check ack from slave */
 
-
-#define I2C_MASTER_SCL_IO           CONFIG_I2C_MASTER_SCL      /*!< GPIO number used for I2C master clock */
-#define I2C_MASTER_SDA_IO           CONFIG_I2C_MASTER_SDA      /*!< GPIO number used for I2C master data  */
-#define I2C_MASTER_NUM              0                          /*!< I2C master i2c port number, the number of i2c peripheral interfaces available will depend on the chip */
-#define I2C_MASTER_FREQ_HZ          400000                     /*!< I2C master clock frequency */
-#define I2C_MASTER_TX_BUF_DISABLE   0                          /*!< I2C master doesn't need buffer */
-#define I2C_MASTER_RX_BUF_DISABLE   0                          /*!< I2C master doesn't need buffer */
-#define I2C_MASTER_TIMEOUT_MS       1000
-
-#define MPU6050_ADDR                0x68
+/*-----MPU6050 configs-----*/
+#define MPU6050_ADDR                  0x68
 #define MPU6050_SMPLRT_DIV_REGISTER   0x19
 #define MPU6050_CONFIG_REGISTER       0x1a
 #define MPU6050_GYRO_CONFIG_REGISTER  0x1b
@@ -26,156 +23,223 @@ static const char *TAG = "i2c-simple-example";
 #define MPU6050_PWR_MGMT_1_REGISTER   0x6b
 #define MPU6050_GYRO_OUT_REGISTER     0x43
 #define MPU6050_ACCEL_OUT_REGISTER    0x3B
-
-// #define ACK_CHECK_EN 0x1            /*!< I2C master will check ack from slave*/
-// #define WRITE_BIT I2C_MASTER_WRITE  /*!< I2C master write */
-// #define READ_BIT I2C_MASTER_READ    /*!< I2C master read */
-// #define I2C_MASTER_TX_BUF_DISABLE 0 /*!< I2C master doesn't need buffer */
-// #define I2C_MASTER_RX_BUF_DISABLE 0 /*!< I2C master doesn't need buffer */
-// #define SCL_IO_PIN CONFIG_I2C_MASTER_SCL
-// #define SDA_IO_PIN CONFIG_I2C_MASTER_SDA
-// #define MASTER_FREQUENCY CONFIG_I2C_MASTER_FREQUENCY
-// #define PORT_NUMBER -1
-// #define LENGTH 48
-// #define TAG "Test"
-// #define BUFF_SIZE 16
-
-static gpio_num_t i2c_gpio_sda = 18;
-static gpio_num_t i2c_gpio_scl = 19;
-static uint32_t i2c_frequency = 100000;
-
-static i2c_port_t i2c_port = I2C_NUM_0;
-
-uint8_t write_buf[2] = {};
-
-float acc_lsb_to_g = 16384.0;
-
-// uint8_t data[BUFF_SIZE];
-
-// static esp_err_t i2c_get_port(int port, i2c_port_t *i2c_port)
-// {
-//     if (port >= I2C_NUM_MAX) {
-//         ESP_LOGE(TAG, "Wrong port number: %d", port);
-//         return ESP_FAIL;
-//     }
-//     *i2c_port = port;
-//     return ESP_OK;
-// }
-
-// static esp_err_t i2c_master_driver_initialize(void)
-// {
-//     i2c_config_t conf = {
-//         .mode = I2C_MODE_MASTER,
-//         .sda_io_num = i2c_gpio_sda,
-//         .sda_pullup_en = GPIO_PULLUP_ENABLE,
-//         .scl_io_num = i2c_gpio_scl,
-//         .scl_pullup_en = GPIO_PULLUP_ENABLE,
-//         .master.clk_speed = i2c_frequency,
-//         // .clk_flags = 0,          /*!< Optional, you can use I2C_SCLK_SRC_FLAG_* flags to choose i2c source clock here. */
-//     };
-//     return i2c_param_config(i2c_port, &conf);
-// }
-
-// static int do_i2cdetect_cmd()
-// {
-//     i2c_driver_install(i2c_port, I2C_MODE_MASTER, I2C_MASTER_RX_BUF_DISABLE, I2C_MASTER_TX_BUF_DISABLE, 0);
-//     i2c_master_driver_initialize();
-//     uint8_t address;
-//     printf("     0  1  2  3  4  5  6  7  8  9  a  b  c  d  e  f\r\n");
-
-//     for (int i = 0; i < 128; i += 16) {
-//         printf("%02x: ", i);
-//         for (int j = 0; j < 16; j++) {
-//             fflush(stdout);
-//             address = i + j;
-//             i2c_cmd_handle_t cmd = i2c_cmd_link_create();
-//             i2c_master_start(cmd);
-//             i2c_master_write_byte(cmd, (address << 1) | WRITE_BIT, ACK_CHECK_EN);
-//             i2c_master_stop(cmd);
-//             esp_err_t ret = i2c_master_cmd_begin(i2c_port, cmd, 50 / portTICK_PERIOD_MS);
-//             i2c_cmd_link_delete(cmd);
-
-//             if (ret == ESP_OK) {
-//                 printf("%02x ", address);
-//                 i2c_master_read_byte(cmd,data, I2C_MASTER_LAST_NACK);
-//                 //printf("Dados lidos: 0x%02x\n", data[0]);
-
-//             } else if (ret == ESP_ERR_TIMEOUT) {
-//                 printf("UU ");
-//             } else {
-//                 printf("-- ");
-//             }
-//         }
-//         printf("\r\n");
-//     }
-
-//     i2c_driver_delete(i2c_port);
-//     return 0;
-// }
+#define RAD_2_DEG             57.29578 // [deg/rad]
+#define CALIB_OFFSET_NB_MES   500
+#define TEMP_LSB_2_DEGREE     340.0    // [bit/celsius]
+#define TEMP_LSB_OFFSET       12412.0
+#define DEFAULT_GYRO_COEFF    0.98
 
 /**
- * @brief i2c master initialization
+ * @brief Initialize I2C hardware with 400kHz clock speed
+ * 
  */
-static esp_err_t i2c_master_init(void) {
-    int i2c_master_port = I2C_MASTER_NUM;
+void i2c_init();
 
-    i2c_config_t conf = {
+/**
+ * @brief Write value to device's register through I2C
+ * 
+ * @param i2c_num I2C port num
+ * @param addr I2C device address
+ * @param reg register address
+ * @param value register value
+ * @return esp_err_t 
+ * 
+ * __________________________________________________________________________________
+ * | start | slave_addr + wr_bit + ack | write reg + ack | write value + ack | stop |
+ * |_______|___________________________|_________________|___________________|______|
+*/
+esp_err_t i2c_write_command(i2c_port_t, uint8_t, uint8_t, uint8_t);
+
+/**
+ * @brief Read data from register through I2C
+ * 
+ * @param i2c_num I2C port num
+ * @param addr I2C device address
+ * @param reg register
+ * @param data_rd data read array
+ * @param size size of data read array
+ * @return esp_err_t 
+ * 
+ * _______________________________________________________________________________________
+ * | start | slave_addr + rd_bit +ack | read n-1 bytes + ack | read 1 byte + nack | stop |
+ * |_______|__________________________|______________________|____________________|______|
+ */
+esp_err_t i2c_read_data(i2c_port_t, uint8_t, uint8_t, uint8_t*, size_t);
+
+/**
+ * @brief Initialiaze MPU6050 and set working mode
+ * 
+ * @return esp_err_t 
+ */
+esp_err_t mpu_init();
+
+/**
+ * @brief Write value to MPU6050 register through I2C
+ * 
+ * @param reg MPU6050 register address
+ * @param value MPU6050 register value
+ * @return esp_err_t 
+ */
+esp_err_t mpu_write_command(uint8_t, uint8_t);
+
+/**
+ * @brief Fetch data(read raw data) from the MPU6050
+ * 
+ * @return esp_err_t 
+ */
+esp_err_t mpu_fetch();
+
+void i2c_init() {
+    i2c_config_t i2c_conf = {
         .mode = I2C_MODE_MASTER,
-        .sda_io_num = i2c_gpio_sda,
-        .scl_io_num = i2c_gpio_scl,
+        .sda_io_num = I2C_MASTER_SDA_IO,         
         .sda_pullup_en = GPIO_PULLUP_ENABLE,
+        .scl_io_num = I2C_MASTER_SCL_IO,        
         .scl_pullup_en = GPIO_PULLUP_ENABLE,
-        .master.clk_speed = I2C_MASTER_FREQ_HZ,
+        .master.clk_speed = I2C_MASTER_FREQ_HZ, 
+        .clk_flags = 0,                        
     };
+    
+    ESP_ERROR_CHECK(i2c_param_config(I2C_MASTER_PORT_NUM, &i2c_conf));
 
-    i2c_param_config(i2c_master_port, &conf);
-
-    return i2c_driver_install(i2c_master_port, conf.mode, I2C_MASTER_RX_BUF_DISABLE, I2C_MASTER_TX_BUF_DISABLE, 0);
+    ESP_ERROR_CHECK(i2c_driver_install(I2C_MASTER_PORT_NUM, i2c_conf.mode, I2C_MASTER_RX_BUF_DISABLE, I2C_MASTER_TX_BUF_DISABLE, 0));
 }
 
-void app_main(void){
-    // do_i2cdetect_cmd();
+esp_err_t i2c_write_command(i2c_port_t i2c_num, uint8_t addr, uint8_t reg, uint8_t value) {
+    i2c_cmd_handle_t cmd = i2c_cmd_link_create();
     
-    uint8_t data[2] = {0x00, 0x01};
-    ESP_ERROR_CHECK(i2c_master_init());
-    ESP_LOGI(TAG, "I2C initialized successfully");
+    /**
+     * MPU6050 I2C single write sequence 
+     * 
+     * _______________________________________________________
+     * | Master | S | AD+W |     | RA |     | DATA |     | P |
+     * |________|___|______|_____|____|_____|______|_____|___|
+     * | Slave  |   |      | ACK |    | ACK |      | ACK |   |
+     * |________|___|______|_____|____|_____|______|_____|___|
+     * 
+     * @param S Start Condition: SDA goes from high to low while SCL is high
+     * @param AD Slave I2C address
+     * @param W Write bit (0)
+     * @param R Read bit (1)
+     * @param ACK Acknowledge: SDA line is low while the SCL line is high at the 9th clock cycle
+     * @param NACK Not-Acknowledge: SDA line stays high at the 9th clock cycle
+     * @param RA MPU-60X0 internal register address
+     * @param DATA Transmit or received data
+     * @param P Stop condition: SDA going from low to high while SCL is high
+     */
+    i2c_master_start(cmd);
+    i2c_master_write_byte(cmd, ((addr << 1) | I2C_MASTER_WRITE), ACK_CHECK_EN);
+    i2c_master_write_byte(cmd, reg, ACK_CHECK_EN);
+    i2c_master_write_byte(cmd, value, ACK_CHECK_EN);
+    i2c_master_stop(cmd);
+    
+    esp_err_t ret = i2c_master_cmd_begin(i2c_num, cmd, pdMS_TO_TICKS(1000));
+    i2c_cmd_link_delete(cmd);
+    
+    return ret;
+}
 
+esp_err_t i2c_read_data(i2c_port_t i2c_num, uint8_t addr, uint8_t reg, uint8_t *data_rd, size_t size) {
+    
+    if (size == 0) {
+        return ESP_OK;
+    }
 
-    int ret;
-    // begin ----------------------------------------
-    write_buf[0] = MPU6050_PWR_MGMT_1_REGISTER;
-    write_buf[1] = 0x01;
-    ret = i2c_master_write_to_device(I2C_MASTER_NUM, MPU6050_ADDR, write_buf, sizeof(write_buf), I2C_MASTER_TIMEOUT_MS / portTICK_PERIOD_MS);
-    ESP_LOGW(TAG, "i2c return code: %i", ret);
+    /**
+     * MPU6050 I2C burts read sequence 
+     * 
+     * _______________________________________________________________________________________
+     * | Master | S | AD+W |     | RA |     | S | AD+R |      |      | ACK |      | NACK | P |
+     * |________|___|______|_____|____|_____|___|______|______|______|_____|______|______|___|
+     * | Slave  |   |      | ACK |    | ACK |   |      | ACK  | DATA |     | DATA |      |   |
+     * |________|___|______|_____|____|_____|___|______|______|______|_____|______|______|___|
+     * 
+     * @param S Start Condition: SDA goes from high to low while SCL is high
+     * @param AD Slave I2C address
+     * @param W Write bit (0)
+     * @param R Read bit (1)
+     * @param ACK Acknowledge: SDA line is low while the SCL line is high at the 9th clock cycle
+     * @param NACK Not-Acknowledge: SDA line stays high at the 9th clock cycle
+     * @param RA MPU-60X0 internal register address
+     * @param DATA Transmit or received data
+     * @param P Stop condition: SDA going from low to high while SCL is high
+     */
+    i2c_cmd_handle_t cmd = i2c_cmd_link_create();
+    i2c_master_start(cmd);
+    i2c_master_write_byte(cmd, (addr << 1) | I2C_MASTER_WRITE, ACK_CHECK_EN);
+    i2c_master_write_byte(cmd, reg, ACK_CHECK_EN);
+    i2c_master_start(cmd);
+    i2c_master_write_byte(cmd, (addr << 1) | I2C_MASTER_READ, ACK_CHECK_EN);
+    if (size > 1) {
+        i2c_master_read(cmd, data_rd, size - 1, I2C_MASTER_ACK); // read (size - 1) bytes
+    }
+    i2c_master_read_byte(cmd, data_rd + size - 1, I2C_MASTER_LAST_NACK);
+    i2c_master_stop(cmd);
+    
+    esp_err_t ret = i2c_master_cmd_begin(i2c_num, cmd, 3000 / portTICK_PERIOD_MS);
+    i2c_cmd_link_delete(cmd);
+    
+    return ret;
+}
 
-    write_buf[0] = MPU6050_SMPLRT_DIV_REGISTER;
-    write_buf[1] = 0x00;
-    ret = i2c_master_write_to_device(I2C_MASTER_NUM, MPU6050_ADDR, write_buf, sizeof(write_buf), I2C_MASTER_TIMEOUT_MS / portTICK_PERIOD_MS);
-    ESP_LOGW(TAG, "i2c return code: %i", ret);
+esp_err_t mpu_init() {
+    i2c_init();
 
-    write_buf[0] = MPU6050_CONFIG_REGISTER;
-    write_buf[1] = 0x00;
-    ret = i2c_master_write_to_device(I2C_MASTER_NUM, MPU6050_ADDR, write_buf, sizeof(write_buf), I2C_MASTER_TIMEOUT_MS / portTICK_PERIOD_MS);
-    ESP_LOGW(TAG, "i2c return code: %i", ret);
-    // ------------------------------------------------
+    esp_err_t ret = mpu_write_command(MPU6050_PWR_MGMT_1_REGISTER, 0x00);
+    mpu_write_command(MPU6050_SMPLRT_DIV_REGISTER, 0x00);
+    mpu_write_command(MPU6050_CONFIG_REGISTER, 0x00);
+
+    mpu_write_command(MPU6050_GYRO_CONFIG_REGISTER, 0x00);
+    mpu_write_command(MPU6050_ACCEL_CONFIG_REGISTER, 0x00);
+
+    ESP_LOGW(TAG, "i2c ret=0x%x", ret);
+    ESP_LOGW(TAG, "i2c ret=%i", ret);
+
+    vTaskDelay(2000 / portTICK_PERIOD_MS);
+
+    return ret;
+}
+
+esp_err_t mpu_write_command(uint8_t reg, uint8_t value) {
+    return i2c_write_command(I2C_MASTER_PORT_NUM, MPU6050_ADDR, reg, value);
+}
+
+esp_err_t mpu_fetch() {
+    uint8_t raw_data[14] = {};
+
+    esp_err_t ret = i2c_read_data(I2C_MASTER_PORT_NUM, MPU6050_ADDR, MPU6050_ACCEL_OUT_REGISTER, raw_data, sizeof(raw_data));
+
+    int16_t raw_acc_x = (raw_data[0] << 8) + raw_data[1];
+
+    float acc_x = ((float)raw_acc_x) / 16384.0;
+
+    ESP_LOGI(TAG, "acc_x=%f", acc_x);
+    ESP_LOGW(TAG, "[0]:%x, [1]%x, raw:%i", raw_data[0], raw_data[1], raw_acc_x);
+
+    int16_t raw_temp = (raw_data[6] << 8) + raw_data[7];
+
+    float temp = ((float)raw_temp + TEMP_LSB_OFFSET) / TEMP_LSB_2_DEGREE;
+
+    ESP_LOGE(TAG, "temp:%f", temp);
+    ESP_LOGW(TAG, "[6]:%x, [7]%x, raw:%i", raw_data[6], raw_data[7], raw_temp);
+
+    return ret;
+}
+
+void app_main(void) {
+    esp_err_t ret1 = mpu_init();
+    esp_err_t ret2;
+    uint8_t raw_data;
+
+    if (ret1 != ESP_OK) {
+        esp_restart();
+    }
 
     for (;;) {
-        // fetchdata -------------------------------------
-        write_buf[0] = MPU6050_ACCEL_OUT_REGISTER;
-        write_buf[1] = 0x01;
-        // ret = i2c_master_write_to_device(I2C_MASTER_NUM, MPU6050_ADDR, write_buf, sizeof(write_buf), I2C_MASTER_TIMEOUT_MS / portTICK_PERIOD_MS);
-        // ESP_LOGW(TAG, "i2c return code: %i", ret);
-        
-        uint8_t rawData[14];
-        i2c_master_write_read_device(I2C_MASTER_NUM, MPU6050_ADDR, write_buf, sizeof(write_buf), rawData, 14, I2C_MASTER_TIMEOUT_MS / portTICK_PERIOD_MS);
+        ret2 = mpu_fetch();
 
-        int16_t raw2;
-        raw2 = rawData[0] << 8;
-        raw2 |= rawData[1];
-        float acc_x = (float)raw2;
-        acc_x /= acc_lsb_to_g ;
-        ESP_LOGI(TAG, "acc_x: %f", acc_x);
+        ESP_LOGI(TAG, "fetch ret:%x", ret2);
 
-        vTaskDelay(pdMS_TO_TICKS(500));
+        vTaskDelay(pdMS_TO_TICKS(1000));
     }
 }
